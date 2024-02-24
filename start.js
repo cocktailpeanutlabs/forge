c853c7b8484a08e9fff76b16ddc4134c36df9d35
cocktailpeanut <cocktailpeanuts@proton.me>
2024-02-24 11:46:30 -0500

    init

---
+++ start.js
@@ -0,0 +1,25 @@
+module.exports = async (kernel) => {
+  return {
+    daemon: true,
+    run: [{
+      method: "shell.run",
+      params: {
+        path: "app",
+        message: (kernel.platform === 'win32' ? 'webui-user.bat' : 'bash webui.sh -f'),
+        env: { SD_WEBUI_RESTARTING: 1 },
+        on: [{ "event": "/http:\/\/[0-9.:]+/", "done": true }]
+      }
+    }, {
+      method: "local.set",
+      params: {
+        "url": "{{input.event[0]}}",
+      }
+    }, {
+      "method": "proxy.start",
+      "params": {
+        "uri": "{{local.url}}",
+        "name": "Local Sharing"
+      }
+    }]
+  }
+}
