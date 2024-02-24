a9ede62d029ecefb5eb2424a79361b793108a363
cocktailpeanut <cocktailpeanuts@proton.me>
2024-02-24 13:23:46 -0500

    update

--- start.js
+++ start.js
@@ -1,4 +1,8 @@
 module.exports = async (kernel) => {
+  let env = { SD_WEBUI_RESTARTING: 1 }
+  if (kernel.platform === 'darwin' && kernel.arch === 'x64') {
+    env.PYTORCH_MPS_HIGH_WATERMARK_RATIO = 0
+  }
   return {
     daemon: true,
     run: [{
@@ -6,7 +10,7 @@ module.exports = async (kernel) => {
       params: {
         path: "app",
         message: (kernel.platform === 'win32' ? 'webui-user.bat' : 'bash webui.sh -f'),
-        env: { SD_WEBUI_RESTARTING: 1, PYTORCH_MPS_HIGH_WATERMARK_RATIO: 0 },
+        env,
         on: [{ "event": "/http:\/\/[0-9.:]+/", "done": true }]
       }
     }, {
